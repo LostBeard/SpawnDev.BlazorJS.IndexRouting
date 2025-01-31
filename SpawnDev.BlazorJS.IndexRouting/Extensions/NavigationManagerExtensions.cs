cfdb9205d0796b3274301bbcc4fffd2a4ee1ada4
Todd Tanner <todd@spawndev.com>
2025-01-31 10:43:37 -0500

    Add project files.

---
+++ SpawnDev.BlazorJS.IndexRouting/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.Extensions.DependencyInjection;
+
+//namespace SpawnDev.BlazorJS.IndexRouting.Routing;
+
+//public static class NavigationManagerExtensions
+//{
+//    public static void AddIndexRoutingNavigationManager(this IServiceCollection services)
+//    {
+//        services.AddScoped<IIndexRoutingNavigationManager, IndexRoutingNavigationManager>();
+//    }
+
+//    public static void AddCoreNavigationManager(this IServiceCollection services)
+//    {
+//        services.AddScoped<IIndexRoutingNavigationManager, CoreNavigationManager>();
+//    }
+//}
+
